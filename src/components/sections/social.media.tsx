a14fb439e568b9e1aa1866f3a88fb2781551b82e
An Hoàng Diển <leehoang1202@gmail.com>
2025-02-09 07:50:11 +0700

    Update social.media.tsx

--- src/components/sections/social.media.tsx
+++ src/components/sections/social.media.tsx
@@ -1,32 +1,33 @@
 import { FaFacebook } from "react-icons/fa6";
-import { SiUdemy } from "react-icons/si";
 import { FaTiktok } from "react-icons/fa";
 import { SiYoutubeshorts } from "react-icons/si";
 
 interface IProps {
     youtube: string;
     tiktok: string;
-    udemy: string;
     facebook: string;
 }
 const SocialMedia = (props: IProps) => {
-    const { youtube, tiktok, udemy, facebook } = props;
+    const { youtube, tiktok, facebook } = props;
 
     return (
-        <div className="my-4 d-flex items-center gap-3">
-            <a href={youtube} target='_blank' className="highlight" title="Youtube Hỏi Dân IT">
-                <SiYoutubeshorts size={30} />
-            </a>
-            <a href={tiktok} target='_blank' className="highlight" title="Tiktok Hỏi Dân IT">
-                <FaTiktok size={30} />
-            </a>
-            <a href={udemy} target='_blank' className="highlight" title="Udemy Hỏi Dân IT">
-                <SiUdemy size={30} />
-            </a>
-            <a href={facebook} target='_blank' className="highlight" title="Facebook Hỏi Dân IT">
-                <FaFacebook size={30} />
-            </a>
-
+        <div>
+            <div className="my-4 d-flex items-center gap-3">
+                <a href={youtube} target='_blank' className="highlight" title="Youtube Dien An">
+                    <SiYoutubeshorts size={30} />
+                </a>
+                <a href={tiktok} target='_blank' className="highlight" title="Tiktok Dien An">
+                    <FaTiktok size={30} />
+                </a>
+                <a href={facebook} target='_blank' className="highlight" title="Facebook Dien An">
+                    <FaFacebook size={30} />
+                </a>
+    
+            </div>
+            <div>
+                <h6>Phone/Zalo: 0703555544</h6>
+                <h6>Email: anhoangdien12@gmail.com</h6>
+            </div>
         </div>
     )
 }
