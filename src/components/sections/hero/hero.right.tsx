8aaf65cd713300cfe44e31f997836fc68a7a7640
An Hoàng Diển <leehoang1202@gmail.com>
2025-02-09 07:46:20 +0700

    Update hero.right.tsx

--- src/components/sections/hero/hero.right.tsx
+++ src/components/sections/hero/hero.right.tsx
@@ -13,37 +13,31 @@ const HeroRight = () => {
             <div className='second'>
                 <code className="text-white-0">
                     <div>
-                        <span className="me-2 text-pink-100">const</span>
-                        <span className="me-2">developer</span>
+                        <span className="me-2 text-pink-100">Sumary</span>
+                        <span className="me-2">Experiences</span>
                         <span className="me-2 text-pink-100">=</span>
                         <span>{'{'}</span>
                     </div>
                     <div>
                         <span className="ms-4 me-2">name:</span>
                         <span className='text-yellow-500'>
-                            {`'`}Hỏi Dân IT{`',`}
+                            {`'`}An Hoang Dien{`',`}
                         </span>
                     </div>
                     <div className="ms-4 me-2 text-pink-200">
                         <span className="text-white-0">skills:</span>
                         <span>{`['`}</span>
-                        <span >React</span>
+                        <span >Operate E-commerce Platform</span>
                         <span>{"', '"}</span>
-                        <span >Redux</span>
+                        <span >Solving Problem</span>
                         <span>{"', '"}</span>
-                        <span >NextJS</span>
+                        <span >Management Team</span>
                         <span>{"', '"}</span>
-                        <span >Java Spring</span>
+                        <span >Planning</span>
                         <span>{"', '"}</span>
-                        <span >NestJS</span>
+                        <span >SEO</span>
                         <span>{"', '"}</span>
-                        <span >NoSQL</span>
-                        <span>{"', '"}</span>
-                        <span >SQL</span>
-                        <span>{"', '"}</span>
-                        <span >Docker</span>
-                        <span>{"', '"}</span>
-                        <span >K8S</span>
+                        <span >Dara Analysis Dashboard</span>
                         <span>{"'],"}</span>
                     </div>
                     <div>
@@ -62,7 +56,7 @@ const HeroRight = () => {
                         <span>,</span>
                     </div>
                     <div>
-                        <span className="ms-4 me-2 text-green-700">hireable:</span>
+                        <span className="ms-4 me-2 text-green-700">Achievement:</span>
                         <span className="text-red-500">function</span>
                         <span>{'() {'}</span>
                     </div>
@@ -71,13 +65,13 @@ const HeroRight = () => {
                         <span>{`(`}</span>
                     </div>
                     <div>
-                        <span className="ms-5 ps-3 text-blue-100">this.</span>
-                        <span className="me-2 ">hardWorker</span>
-                        <span className='text-blue-100'>&amp;&amp;</span>
+                        <span className="ms-5 ps-3 text-blue-100">Revenue</span>
+                        <span className="me-2 "> E-Com Store from 0</span>
+                        <span className='text-blue-100'>to 800 million vnd</span>
                     </div>
                     <div>
-                        <span className="ms-5 ps-3 text-blue-100">this.</span>
-                        <span className="me-2 ">problemSolver</span>
+                        <span className="ms-5 ps-3 text-blue-100">Buil</span>
+                        <span className="me-2 "> problemSolver</span>
                         <span className='text-blue-100'>&amp;&amp;</span>
                     </div>
                     <div>
